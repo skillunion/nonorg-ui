34f63adb18dd80e5601b75b52b913af88d3b795a
human <dima@ethernian.com>
2019-02-14 01:35:48 +0100

    [+] UNTESTED: newRegistryWithToken_ThenApply()

--- src/store/registries.js
+++ src/store/registries.js
@@ -78,7 +78,51 @@ const actions = {
       address: null,
       name: data.registry.name
     })
+  },
+
+  ADD_REGISTRY_THEN_APPLY: async (context, data) => {
+    var registryReceipt = await Eth.registryFactory_rw.newRegistryWithToken_ThenApply(
+      //ToDo: use other function to parse tokens in wei, taking precision in account other than 18
+      ethers.utils.parseEther(data.registry.token.supply.toString()),
+      data.registry.token.name,
+      data.registry.token.decimals,
+      data.registry.token.symbol,
+      // ============ Apply Listing =========
+      data.listing.hash, 
+      data.listing.amount, 
+      ethers.utils.toUtf8Bytes(data.listing.data)   // use ethers.utils.toUtf8String for decode
+      [
+        ethers.utils.parseEther(data.registry.parameters.minDeposit.toString()),
+        ethers.utils.parseEther(data.registry.parameters.pMinDeposit.toString()),
+        data.registry.parameters.applyStageLength,
+        data.registry.parameters.pApplyStageLength,
+        data.registry.parameters.commitStageLength,
+        data.registry.parameters.pCommitStageLength,
+        data.registry.parameters.revealStageLength,
+        data.registry.parameters.pRevealStageLength,
+        data.registry.parameters.dispensationPct,
+        data.registry.parameters.pDispensationPct,
+        data.registry.parameters.voteQuorum,
+        data.registry.parameters.pVoteQuorum,
+        data.registry.parameters.exitTimeDelay,
+        data.registry.parameters.exitPeriodLen
+      ],
+      data.registry.name
+    );
+
+    context.commit('ADD_REGISTRY', {
+      transactionHash: registryReceipt.hash,
+      address: null,
+      name: data.registry.name
+    })
+
+    context.commit('ADD_WORK', {
+      transactionHash: registryReceipt.hash,
+      address: null,
+      name: data.registry.name
+    })
   }
+
 }
 
 export default {
